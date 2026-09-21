59f370d99524d967a617b285bbead44246dd3d3c
d84662322-byte <d84662322@gmail.com>
2026-09-21 05:07:08 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const existe = new Set();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Middleware de logs
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('¡Servidor funcionando correctamente!');
@@ -21,6 +29,16 @@ app.post('/api/usuario', (req, res) => {
         error: 'Faltan datos obligatorios (key o value)' 
       });
     }
+    
+    if (existe.has(key)) {
+      console.log(`La key ${key} ya fue registrada anteriormente`);
+      return res.status(409).json({
+        error: 'Key ya registrada',
+        mensaje: 'Esta Key ya fue enviada antes'
+      });
+    }
+    
+    existe.add(key);
 
     console.log('Datos recibidos ✅✅✅');
     console.log('key:', key);
